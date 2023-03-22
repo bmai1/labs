9a9002c6c38440846ea29457c58b6909972cf159
Brian Mai <0hrs3g@gmail.com>
2023-03-22 12:39:32 -0700

    Add files via upload

---
+++ lab8A.py
@@ -0,0 +1,11 @@
+read = input("Read from: ")
+write = input("Write to: ")
+
+file1 = open(read, 'r')
+file2 = open(write, 'w')
+
+for line in file1:
+    file2.write(line)
+
+file1.close()
+file2.close()
